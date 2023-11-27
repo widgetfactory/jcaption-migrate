3574cd9ea9cd89c4bfca0aab830a7ebae185653e
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2023-11-27 09:51:15 +0000

    Update jcaption.php for Joomla 4

--- plugins/system/jcaption/jcaption.php
+++ plugins/system/jcaption/jcaption.php
@@ -2,18 +2,23 @@
 
 defined('JPATH_PLATFORM') or die;
 
-class PlgSystemJCaption extends JPlugin
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+
+class PlgSystemJCaption extends CMSPlugin
 {
     public function onAfterDispatch()
     {
-        $app = JFactory::getApplication();
+        $app = Factory::getApplication();
 
         // only in "site"
         if ($app->getClientId() !== 0) {
             return;
         }
 
-        $document = JFactory::getDocument();
+        $document = Factory::getDocument();
         $docType = $document->getType();
 
         // only in html pages
@@ -22,8 +27,8 @@ class PlgSystemJCaption extends JPlugin
         }
 
         // Include jQuery
-        JHtml::_('jquery.framework');
+        HTMLHelper::_('jquery.framework');
 
-        $document->addScript(JURI::base(true) . '/plugins/system/jcaption/js/jcaption.js', array('version'));
+        $document->addScript(Uri::base(true) . '/plugins/system/jcaption/js/jcaption.js', array('version'));
     }
 }
